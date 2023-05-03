55c5628a010df38de721ddbb672518142ccc442e
Shashanksarojj <shashankdw1704@gmail.com>
2023-05-03 13:02:54 +0530

    day-2

--- src/com/masai/Main.java
+++ src/com/masai/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("hii");
         System.out.println("hii");
         System.out.println("hii");
+        System.out.println("Shashank Dwivedi");
 
     }
 }
