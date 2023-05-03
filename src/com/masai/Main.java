b2187825d87262fe7cfc90e1f70488c8808226af
Shashanksarojj <shashankdw1704@gmail.com>
2023-05-03 12:57:37 +0530

    day2

--- src/com/masai/Main.java
+++ src/com/masai/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hii");
         System.out.println("hii");
+        System.out.println("hii");
+        System.out.println("hii");
     }
 }
